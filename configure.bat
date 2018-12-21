467f302b204a318a60293b36891d1e2967f10854
ivan <imartinezp@bbva.com>
2018-12-21 16:30:22 +0100

    1111

--- configure.bat
+++ configure.bat
@@ -2,10 +2,14 @@
                    
 pushd %cd%
 
-c:\Python36\Scripts\virtualenv.exe virtualenv
+c:\Python37\Scripts\virtualenv.exe virtualenv
 call ./virtualenv/Scripts/activate.bat
 rem pip install -i https://pypi.python.org/pypi --proxy http://10.100.192.1:3128/ -r ./requirements.txt
-pip install -r ./requirements.txt 
+rem pip install -r ./requirements.txt
+pip install Click-7.0-py2.py3-none-any.whl 
+pip install Werkzeug-0.14.1-py2.py3-none-any.whl
+pip install Jinja2-2.10-py2.py3-none-any.whl
+pip install Flask-1.0.2-py2.py3-none-any.whl
 pip install WTForms-2.2.1-py2.py3-none-any.whl
 pip install Flask_WTF-0.14.2-py2.py3-none-any.whl
 rem pip install -r ./requirements/dev.txt
